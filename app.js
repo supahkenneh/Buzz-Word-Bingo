5a6644bf01db7a97a62e654064bbeec9c7740317
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-24 17:03:37 -1000

    get

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = process.env.port || 3005;
+
+const words = [];
+const success = { "success" : true };
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.get('/buzzwords', (req, res) => {
+  res.send(words);
+  res.json(success);
+});
+
+app.post(`/buzzwords`, (req, res) => {
+  res.json(success);
+})
 
 app.listen(PORT, () => {
   console.log(`Now listening on port: ${PORT}`)
