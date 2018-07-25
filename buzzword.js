7da65758ec22c9fece5360e479f065fcecc02999
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-24 19:59:27 -1000

    put

--- buzzword.js
+++ buzzword.js
@@ -4,18 +4,31 @@ const bodyParser = require('body-parser');
 const data = require(`./words`);
 
 
-router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-const success = { "success" : true };
+const success = { "success": true };
 
 router.post('/', (req, res) => {
-    let newBuzzword = {};
-    newBuzzword.word = req.body.buzzword;
-    newBuzzword.points = req.body.points;
-    newBuzzword.heard = false;
-    data.wordBank.push(newBuzzword)
-    res.send(success);
+  let newBuzzword = {};
+  newBuzzword.word = req.body.buzzword;
+  newBuzzword.points = req.body.points;
+  newBuzzword.heard = false;
+  data.wordBank.push(newBuzzword)
+  res.send(success);
+});
+
+router.put('/', (req, res) => {
+  for (let i = 0; i < data.wordBank.length; i++) {
+    if (req.body.buzzword === data.wordBank[i].word) {
+      data.wordBank[i].heard = true;
+      data.score += parseInt(req.body.points);
+      res.send(`"success": true, current score: ${data.score}`);
+      return true;
+    }
+  }
+  res.end();
+  return false;
 })
-  
+
 module.exports = router;
--- words.js
+++ words.js
@@ -1,5 +1,7 @@
 const wordBank = [];
+const score = 0;
 
 module.exports = {
   wordBank: wordBank,
+  score: score,
 }
